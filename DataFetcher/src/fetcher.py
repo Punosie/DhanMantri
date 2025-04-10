0e81564372796c34da07cb075de3682d88a45d3d
Shubhankar Kaushik <shubhankar.kaushik2003@gmail.com>
2025-04-10 23:34:46 +0530

    create daily fetch task

--- DataFetcher/src/fetcher.py
+++ DataFetcher/src/fetcher.py
@@ -1,13 +1,17 @@
+from datetime import datetime
 import yfinance as yf
 
-def fetch_ohlcv(symbol, period='max', interval='1d'):
+def fetch_ohlcv(symbol):
     try:
+        today = datetime.now().strftime("%Y-%m-%d")
         ticker = yf.Ticker(symbol)
-        data = ticker.history(period=period, interval=interval)
+        data = ticker.history(start=today, end=today, interval="1d")
+
         if data.empty:
-            print(f"Warning: No data available for {symbol}. Skipping.")
-            return None  # Return None if no data is available
+            print(f"Warning: No data available for {symbol} today. Skipping.")
+            return None
+
         return data
     except Exception as e:
         print(f"Error fetching data for {symbol}: {e}")
-        return None  # Return None on error
+        return None
